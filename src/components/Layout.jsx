8e71ecc2d21a9690f9f689129cddd05e0edbf7b9
anup30 <anupbarua30@gmail.com>
2025-09-17 12:36:56 +0600

    v1.3

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -12,7 +12,7 @@ function Layout({ children }) {
       
       <div className="flex flex-1 pt-16 md:pl-64">
         <main className="flex-1 p-6">
-          <div className="max-w-[1440px] mx-auto my-16">
+          <div className="max-w-[1440px] mx-auto"> {/* my-16 */}
             <div className="bg-white/80 rounded-xl shadow-xl p-6">
               {children}
             </div>

d2598d0b961c9d6e6522a7215a09df3e375c9253
anup30 <anupbarua30@gmail.com>
2025-11-02 13:51:32 +0600

    v2.8

--- src/components/AdminRoute.jsx
+++ src/components/AdminRoute.jsx
@@ -3,7 +3,14 @@ import { Navigate } from 'react-router';
 import useAuthContext from '../hooks/useAuthContext';
 
 function AdminRoute({ children }) {
-  const { user } = useAuthContext();
+  const { user, isLoading } = useAuthContext();
+  if (isLoading) {
+    return (
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        Loading...
+      </div>
+    );
+  } 
   return user && (user.is_staff || user.is_superuser) ? children : <Navigate to="/" />;
 }
 
